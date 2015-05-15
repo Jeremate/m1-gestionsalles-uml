5681c8ae4942b2673177ee58bc9399b8cead83f8
developer <developer@localhost.localdomain>
2015-05-15 11:42:29 +0200

    ajout fonction modifier_batiment

--- m1-gestionsalles-uml/main.py
+++ m1-gestionsalles-uml/main.py
@@ -16,6 +16,11 @@ def rechercher_batiment(no_bat):
 	print(no_bat," ",batiments[no_bat])
 
 
+def modifier_batiment(no_bat,nom,adresse):
+	nouveau_bat = batiment.batiment(nom,adresse)
+	if no_bat in batiments.keys():
+		batiments[no_bat] = nouveau_bat
+
 def supprimer_batiment(no_bat):
 	if no_bat not in batiments.keys():
 		del batiments[no_bat]
@@ -23,7 +28,8 @@ def supprimer_batiment(no_bat):
 def main():
 	ajouter_batiment(1,"bat_1","adresse_1")
 	ajouter_batiment(2,"bat_2","adresse_2")
-	rechercher_batiment(1)
+	modifier_batiment(2,"bat_3","adresse_3")
+	rechercher_batiment(2)
 
 if __name__ == '__main__':
 	main()
