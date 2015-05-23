ea722b826cf22b9c1c1ed4f44e499911ac28cb25
developer <developer@localhost.localdomain>
2015-05-23 18:36:46 +0200

    maj classe materiel et batiment pour respecter les property

--- m1_gestionsalles_uml/patrimoine/materiel.py
+++ m1_gestionsalles_uml/patrimoine/materiel.py
@@ -1,9 +1,20 @@
 class Materiel(object):
+	"""Classe représentant un Materiel.
+
+	Attributs:
+		code_inv : Le code inventaire du materiel
+	"""
+
 	def __init__(self,code_inv):
 		self.code_inv = code_inv
-	def get_id():
-		return self.code_inv
-	def __str__(self):
-		return self.code_inv
+
 	def __repr__(self):
-		return self.code_inv
+		return "{code_inv}".format()
+
+	@property
+	def code_inv(self):
+	    return self._code_inv
+
+	@code_inv.setter
+	def code_inv(self, val):
+		self._code_inv = val
