92680e979951d71a4b5245eef405e8f4e33d3ff5
developer <developer@localhost.localdomain>
2015-05-23 18:40:46 +0200

    maj classe typesalle pour respecter les property

--- m1_gestionsalles_uml/patrimoine/typesalle.py
+++ m1_gestionsalles_uml/patrimoine/typesalle.py
@@ -1,9 +1,20 @@
 class Typesalle(object):
+	"""Classe représentant un type de salle.
+
+	Attributs:
+		nom : nom du type de salle
+	"""
+
 	def __init__(self,nom):
 		self.nom = nom
-	def getnom():
-		return self.nom
-	def __str__(self):
-		return self.nom
+	
 	def __repr__(self):
-		return self.nom
+		return "{nom}".format()
+
+	@property
+	def nom(self):
+	    return self._nom
+
+	@nom.setter
+	def nom(self, val):
+		self._nom = val
