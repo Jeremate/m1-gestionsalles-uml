fde8f0fc9cb884b5e62cbadf344645aca3655aa9
developer <developer@localhost.localdomain>
2015-05-27 15:03:19 +0200

    correction majeur pour la classe GestionAPI

--- m1_gestionsalles_uml/main.py
+++ m1_gestionsalles_uml/main.py
@@ -23,113 +23,116 @@ class GestionAPI(object):
 		self._salles = {}
 
 
-	def recuperer_batiments(self):
+	def batiments(self):
 	    return self._batiments
 
+
+	def ajouter_batiment(self,no_bat, nom, adresse):
+		nouveau_bat = batiment.Batiment(no_bat, nom, adresse)
+		if no_bat not in self._batiments:
+			self._batiments[no_bat] = nouveau_bat
+
+
+	batiments = property(batiments, ajouter_batiment)
 	
+
+	def rechercher_batiment(self, no_bat):
+		if no_bat in self._batiments:
+			return self._batiments[no_bat]
+
+
+	def modifier_batiment(self, no_bat, nom, adresse):
+		if no_bat in self._batiments:
+			nouveau_bat = batiment.Batiment(no_bat, nom, adresse)
+			self._batiments[no_bat] = nouveau_bat
+
+	def supprimer_batiment(self, no_bat):
+		if no_bat not in self._batiments:
+			del self._batiments[no_bat]
+
+
+	#fonctionnalitées typesalle
+	def typesalles(self):
+	    return self._typesalles
+
+	def ajouter_typesalle(self, nom_typesalle, description):
+		if nom_typesalle not in self._typesalles:
+			nouveau_typesalle = typesalle.Typesalle(description)
+			self._typesalles[nom_typesalle] = nouveau_typesalle
+
+	def supprimer_typesalle(self, nom_typesalle):
+		if nom_typesalle in self._typesalles:
+			del self._typesalles[nom_typesalle]
+
+	def consulter_typesalle(self, description):
+		if nom_typesalle in self._typesalles:
+			return self._typesalles[nom_typesalle]
+
+	typesalles = property(typesalles, ajouter_typesalle)
+
+
+	#fonctionnalitées materiel
 
 	def materiels(self):
 	    return self._materiels
 
-	@property
-	def typesalles(self):
-	    return self._typesalles
-
-	@property
-	def salles(self):
-	    return self._salles
-
-	@property
-	def typemateriels(self):
-	    return self._typemateriels
-
-
-	def ajouter_batiment(self,no_bat,nom,adresse):
-		nouveau_bat = batiment.Batiment(no_bat,nom,adresse)
-		if no_bat not in self._batiments:
-			self._batiments[no_bat] = nouveau_bat
-
-	batiments = property(recuperer_batiments, ajouter_batiment)
-	
-
-	def rechercher_batiment(self, no_bat):
-		if no_bat in self.batiments:
-			return self.batiments[no_bat]
-
-
-	def modifier_batiment(self, no_bat, nom, adresse):
-		if no_bat in self.batiments:
-			nouveau_bat = batiment.Batiment(nom, adresse)
-			batiments[no_bat] = nouveau_bat
-
-	def supprimer_batiment(self, no_bat):
-		if no_bat not in self.batiments:
-			del self.batiments[no_bat]
-
-
-	#fonctionnalitées typesalle
-	def ajouter_typesalle(self, nom_typesalle, description):
-		if nom_typesalle not in self.typesalles:
-			nouveau_typesalle = typesalle.Typesalle(description)
-			self.typesalles[nom_typesalle] = nouveau_typesalle
-
-	def supprimer_typesalle(self, nom_typesalle):
-		if nom_typesalle in self.typesalles:
-			del self.typesalles[nom_typesalle]
-
-	def consulter_typesalle(self, description):
-		if nom_typesalle in self.typesalles:
-			return self.typesalles[nom_typesalle]
-
-
-	#fonctionnalitées materiel
 	def ajouter_materiel(self, code_inv):
-		if code_inv not in self.materiels:
+		if code_inv not in self._materiels:
 			nouveau_materiel = materiel.Materiel(code_inv)
-			self.materiels[code_inv] = nouveau_materiel
+			self._materiels[code_inv] = nouveau_materiel
 
 	def supprimer_materiel(self, code_inv):
-		if code_inv in self.materiels:
-			del self.materiels[code_inv]
+		if code_inv in self._materiels:
+			del self._materiels[code_inv]
 
 	def consulter_materiel(self, code_inv):
-		if code_inv in self.materiels:
-			return self.materiels[code_inv]
+		if code_inv in self._materiels:
+			return self._materiels[code_inv]
+
+	materiels = property(materiels,ajouter_materiel)
 	#--------------------------------
 
 
 	#fonctionnalitées typemateriel
+	def typemateriels(self):
+	    return self._typemateriels
+
 	def ajouter_typemateriel(self, nom):
-		if nom not in self.typemateriels:
+		if nom not in self._typemateriels:
 			nouveau_typemateriel = typemateriel.Typemateriel(nom)
-			self.typemateriels[nom] = nouveau_typemateriel
+			self._typemateriels[nom] = nouveau_typemateriel
 
 	def supprimer_typemateriel(self, nom):
-		if nom in self.typemateriels:
-			del self.typemateriels[nom]
+		if nom in self._typemateriels:
+			del self._typemateriels[nom]
 
 	def consulter_typemateriel(self, nom):
-		if nom in self.typemateriels:
-			return self.typemateriels[nom]
+		if nom in self._typemateriels:
+			return self._typemateriels[nom]
 
-
+	typesalles = property(typemateriels,ajouter_typemateriel)
 
 	#fonctionnalitées salle
 	#fonction d'ajout d'une salle si le batiment ou le typesalle n'existe pas alors la fonction est abandonné
+	def salles(self):
+	    return self._salles
+
 	def ajouter_salle(self,id_salle,no_etage,no_salle,no_bat,superficie,nom_typesalle):
-		if id_salle not in self.salles:
-			if no_bat in batiments:
-				if nom_typesalle in typesalles:
+		if id_salle not in self.s_alles:
+			if no_bat in self._batiments:
+				if nom_typesalle in self._typesalles:
 					nouvelle_salle = salle.Salle(no_etage,no_salle,no_bat,superficie,typesalle)
-					self.salles[id_salle] = nouvelle_salle
+					self._salles[id_salle] = nouvelle_salle
 
 	#fonction pour supprimer une salle
 	def supprimer_salle(self, id_salle):
-		if id_salle in self.salles:
-			for r in reservations:
+		if id_salle in self._salles:
+			for r in self._reservations:
 				if r[id_salle].id_salle() == id_salle:
-					del reservations[id_salle]
-			del salles[id_salle]
+					del self._reservations[id_salle]
+			del self._salles[id_salle]
+
+	salles = property(salles,ajouter_salle)
 	#-------------------------------
 
 #fonctionnalitées pour la classe demandeur
@@ -140,9 +143,9 @@ def ajouter_demandeur(no_dem, nom):
 def main():
 
 	systeme = GestionAPI()
-	#systeme.ajouter_batiment(1,"Batiment 1","Adresse 1")
-	#systeme.supprimer_batiment(1)
-	#print (batiments.rechercher_batiment(1))
+	systeme.ajouter_batiment(1,"Batiment 1","Adresse 1")
+	systeme.supprimer_batiment(1)
+	print (systeme.rechercher_batiment(1))
 
 	print (systeme.batiments)
 
