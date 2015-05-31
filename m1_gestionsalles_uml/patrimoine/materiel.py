9029c9a868c4f2dd657dfd7c7feabffaec595b87
Jeremate <jeremate@gmail.com>
2015-05-31 12:03:14 +0200

    correction du Materiel

--- m1_gestionsalles_uml/patrimoine/materiel.py
+++ m1_gestionsalles_uml/patrimoine/materiel.py
@@ -1,17 +1,15 @@
-from finance.tabletarif import TableTarif
-
-class Materiel(TableTarif):
+class Materiel(object):
 	"""Classe représentant un Materiel.
 
 	Attributs:
 		code_inv : Le code inventaire du materiel
+		typemateriel : L'identifiant du type du matériel
 	"""
 
-	def __init__(self, code_inv, libelle, montant):
-		super(Materiel, self).initialisation(code_inv, libelle, montant)
+	def __init__(self, code_inv, id_typemateriel):
+		super(Materiel, self).__init__()
 		self.code_inv = code_inv
-
-
+		self.typemateriel = id_typemateriel
 
 	def __str__(self):
 		return "{}".format(self._code_inv)
@@ -26,3 +24,11 @@ class Materiel(TableTarif):
 	@code_inv.setter
 	def code_inv(self, val):
 		self._code_inv = val
+
+	@property
+	def typemateriel(self):
+	    return self._typemateriel
+
+	@code_inv.setter
+	def typemateriel(self, val):
+		self._typemateriel = val
