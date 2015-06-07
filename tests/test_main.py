7a64ca0457acbc7195fa81e75575dcead9e4a35c
developer <developer@localhost.localdomain>
2015-06-07 16:36:12 +0200

    modification majeur

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,4 +6,5 @@ class TestGestionAPI(object):
 	def test_ajouter_adresse(self):
 		systeme = GestionAPI()
 		systeme.ajouter_adresse("Adresse 1", 101, "rue général buat", "44000", "Nantes")
-		assert  systeme.adresses == "{'Adresse 1': 101 rue général buat, 44000 - NANTES}"
+		test_adresses = {"Adresse 1", 101, "rue général buat", "44000", "Nantes"}
+		assert assertDictEqual(systeme.adresses, test_adresses)
